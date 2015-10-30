df6122191a34839ebdbc21f65980b1e6af5f9673
Sarah Kwak <sarahkwak@hotmail.com>
2015-10-30 15:51:21 -0700

    Basic layout for the main page set. Add image column to Brand table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030173718) do
+ActiveRecord::Schema.define(version: 20151030224227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151030173718) do
     t.integer  "perk_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "brands", ["perk_id"], name: "index_brands_on_perk_id", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-50.times { brand = Brand.create([name: Faker::Company.name, location: Faker::Address.postcode, perk_id: rand(50)]) }
-50.times { perk = Perk.create([details: Faker::Commerce.product_name])}
-20.times { category = Category.create([name: Faker::Commerce.department(1)]) }
-10.times { perk_category = PerkCategory.create([perk_id: rand(50), category_id: rand(20)]) }
-favorite = Favorite.create([user_id: 1, perk_category_id: rand(10)])
+# 50.times { brand = Brand.create([name: Faker::Company.name, location: Faker::Address.postcode, perk_id: rand(50)]) }
+# 50.times { perk = Perk.create([details: Faker::Commerce.product_name])}
+# 20.times { category = Category.create([name: Faker::Commerce.department(1)]) }
+# 10.times { perk_category = PerkCategory.create([perk_id: rand(50), category_id: rand(20)]) }
+# favorite = Favorite.create([user_id: 1, perk_category_id: rand(10)])
 
-
+for i in 1..(Brand.count-1)
+  brand = Brand.find(i)
+  brand.update(image: Faker::Avatar.image)
+end 
